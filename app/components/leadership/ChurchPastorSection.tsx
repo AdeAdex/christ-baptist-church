8ee0ea356fb0c29fcb94e15397ee8d6b9c9b6771
Adeolu Amole <adeoluamole@gmail.com>
2025-01-11 13:36:30 +0100

    updates

--- app/components/leadership/ChurchPastorSection.tsx
+++ app/components/leadership/ChurchPastorSection.tsx
@@ -1,20 +1,27 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Image from 'next/image';
-import { Pastor } from '@/app/types/pastors';
+import React from "react";
+import Image from "next/image";
+import { Pastor } from "@/app/types/pastors";
 
 interface ChurchPastorSectionProps {
   pastor: Pastor | null;
 }
 
-const ChurchPastorSection: React.FC<ChurchPastorSectionProps> = ({ pastor }) => {
+const ChurchPastorSection: React.FC<ChurchPastorSectionProps> = ({
+  pastor,
+}) => {
   return (
     <div className="flex flex-col md:flex-row items-center mb-16 gap-4 md:gap-0">
       <div className="w-full md:w-1/2 text-left md:pr-8 order-2 md:order-1">
-      {pastor?.bio?.map((paragraph, index) => (
-          <p key={index} className="text-lg text-gray-600 dark:text-gray-200 mb-4">
-            {paragraph.children.map((child: { text: string }) => child.text).join('')}
+        {pastor?.bio?.map((paragraph, index) => (
+          <p
+            key={index}
+            className="text-lg text-gray-600 dark:text-gray-200 mb-4"
+          >
+            {paragraph.children
+              .map((child: { text: string }) => child.text)
+              .join("")}
           </p>
         ))}
       </div>
