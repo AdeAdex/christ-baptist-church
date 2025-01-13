1c8436eeadc49bdfd22b06407a04d4ec2935ea49
Adeolu Amole <adeoluamole@gmail.com>
2025-01-13 15:27:05 +0100

    fixed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,29 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex items-center justify-center">
-      <div className="text-center text-white p-8 md:p-16">
-        {/* 404 Title with Tailwind fade-in animation */}
-        <h1 className="text-6xl md:text-8xl font-extrabold tracking-tight mb-4 ">
-          4<span className="text-yellow-300">0</span>4
-        </h1>
-        {/* Subtitle with fade-in */}
-        <h2 className="text-3xl md:text-4xl font-semibold mb-6 ">
-          Oops! Page Not Found.
-        </h2>
-        {/* Description with fade-in */}
-        <p className="text-lg mb-8 ">
-          The page you are looking for might have been moved, deleted, or never
-          existed.
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="text-center">
+        
+        <div className="mt-8">
+          <Image
+            src="/images/404-error.jpg"
+            alt="Not Found Illustration"
+            width={400}
+            height={300}
+            className="mx-auto"
+            priority
+          />
+        </div>
+
+        <p className="mt-2 text-lg text-gray-400">
+          The page you are looking for doesn't exist or has been moved.
         </p>
-        {/* Button with hover effect */}
-        <div className="flex justify-center ">
-          <Link
-            href="/"
-            className="px-6 py-3 text-xl font-semibold bg-yellow-300 text-gray-800 rounded-lg shadow-md hover:bg-yellow-400 transition-all duration-300 ease-in-out"
-          >
-            Go Back to Home
+        <div className="mt-6">
+          <Link href="/" className="px-6 py-3 bg-cyan-600 text-white font-medium text-lg rounded-lg shadow-lg hover:bg-cyan-500 transition duration-300">
+              Go Back to Home
           </Link>
         </div>
       </div>
