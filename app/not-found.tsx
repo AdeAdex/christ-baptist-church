e36e05064f06d46729b7f153fa811e7441249d15
Adeolu Amole <adeoluamole@gmail.com>
2025-01-13 15:49:13 +0100

    fixed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -19,7 +19,7 @@ const NotFound = () => {
         </div>
 
         <p className="mt-2 text-lg text-gray-400">
-          The page you are looking for doesn't exist or has been moved.
+          The page you are looking for doesn&rsquo;t exist or has been moved.
         </p>
         <div className="mt-6">
           <Link href="/" className="px-6 py-3 bg-cyan-600 text-white font-medium text-lg rounded-lg shadow-lg hover:bg-cyan-500 transition duration-300">
