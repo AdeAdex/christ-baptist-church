957cd1096beffa241b678568397f45dd627ad57b
AdeAdex <adeoluamole@gmail.com>
2025-03-21 12:02:33 +0100

    updates to the login button on the navbar

--- app/components/navbar/AuthMenu.tsx
+++ app/components/navbar/AuthMenu.tsx
@@ -115,11 +115,11 @@ const AuthMenu = () => {
         </Menu>
       ) : (
         <Link
-          href="/login"
-          className="border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition"
-        >
-          Login
-        </Link>
+        href="/login"
+        className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+      >
+        Login
+      </Link>
       )}
 
       {/* Theme Toggle */}
