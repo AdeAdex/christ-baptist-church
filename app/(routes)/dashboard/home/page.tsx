d8d9bac3a4ef6067eaf1a8fedba78174dedc1477
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2025-04-03 06:35:26 +0100

    Update page.tsx

--- app/(routes)/dashboard/home/page.tsx
+++ app/(routes)/dashboard/home/page.tsx
@@ -20,12 +20,11 @@ export default function HomePage() {
           className="flex flex-col px-4 mb-5 text-white py-4"
           style={{ backgroundColor: "#030552", borderRadius: "5px" }}
         >
-          <h6 className="uppercase flex gap-2 font-bold md:text-xl">
-            Welcome to your dashboard:{" "}
-            <span>
-              {member?.firstName} {member?.lastName}
-            </span>
-          </h6>
+          <h6 className="uppercase inline-flex items-center gap-2 font-bold md:text-xl whitespace-nowrap">
+  Welcome to your dashboard: 
+  <span>{member?.firstName} {member?.lastName}</span>
+</h6>
+
           <span className="my-3">
             You created an account with us on: {formattedDate}
           </span>
