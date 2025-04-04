13a7fb0c25b5b59d0759083c1c2aa32d7a8c0f1c
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2025-04-04 21:17:02 +0100

    Update contributionSummary.model.ts

--- app/models/contributionSummary.model.ts
+++ app/models/contributionSummary.model.ts
@@ -1,11 +1,14 @@
 // /app/models/contributionSummary.model.ts
+
+
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IContributionSummary extends Document {
-  member: mongoose.Types.ObjectId;
+  member: mongoose.Types.ObjectId; // ChurchMember ID
   month: string;
   year: number;
   totalAmount: number;
+  totalMonthlyContribution: number;  // New field for total monthly contribution
 }
 
 const ContributionSummarySchema = new Schema<IContributionSummary>(
@@ -13,7 +16,8 @@ const ContributionSummarySchema = new Schema<IContributionSummary>(
     member: { type: Schema.Types.ObjectId, ref: "ChurchMember", required: true },
     month: { type: String, required: true },
     year: { type: Number, required: true },
-    totalAmount: { type: Number, default: 0 },
+    totalAmount: { type: Number, required: true },
+    totalMonthlyContribution: { type: Number, required: true, default: 0 },  // Add totalMonthlyContribution
   },
   { timestamps: true }
 );
