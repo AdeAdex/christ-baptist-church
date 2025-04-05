d75b8f25fadc4b9a82204f148f582f2bfb7d7c4c
Adeolu Amole <98040608+AdeAdex@users.noreply.github.com>
2025-04-05 10:36:30 +0100

    Update useActivities.ts

--- app/hooks/admin/useActivities.ts
+++ app/hooks/admin/useActivities.ts
@@ -1,33 +1,3 @@
-// // /app/hooks/admin/useActivities.ts
-
-// import { useEffect } from "react";
-// import { useSelector } from "react-redux";
-// import { RootState } from "@/app/redux/store";
-// import { useAppDispatch } from "@/app/redux/hooks";
-// import { fetchActivities } from "@/app/actions/admin/activityActions";
-// import { Activity } from "@/app/redux/slices/activitiesSlice";
-
-// // export const useActivities = () => {
-// //   const dispatch = useAppDispatch();
-
-// //   // Listening to the activities state in Redux
-// //   const { activities, error } = useSelector(
-// //     (state: RootState) => state.activities
-// //   );
-
-// //   useEffect(() => {
-// //     if (activities.length === 0) {
-// //       dispatch(fetchActivities()); // Fetch activities when they are empty
-// //     }
-// //   }, [dispatch]); // Make sure this effect only runs when `dispatch` changes, to avoid multiple fetch calls
-
-// //   // Return the activities and error to the component
-// //   return { activities, error, dispatch };
-// // };
-
-
-
-
 
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -35,20 +5,19 @@ import { RootState } from "@/app/redux/store";
 import { useAppDispatch } from "@/app/redux/hooks";
 import { fetchActivities } from "@/app/actions/admin/activityActions";
 import { Activity } from "@/app/redux/slices/activitiesSlice";
+import { setLoading } from "@/app/redux/slices/activitiesSlice";
 
 export const useActivities = () => {
   const dispatch = useAppDispatch();
   const activities = useSelector((state: RootState) => state.activities.activities);
   const member = useSelector((state: RootState) => state.auth.member);
-  const [loading, setLoading] = useState(true);
 
   // console.log("Filtered Activities:", activities);
 
   useEffect(() => {
     const fetchActivitiesData = async () => {
-      setLoading(true); // Set loading to true when fetching
+      dispatch(setLoading());
       await dispatch(fetchActivities());
-      setLoading(false); // Set loading to false once fetching is complete
     };
 
     fetchActivitiesData();
